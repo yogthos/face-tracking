da869e1eeca3eb7495d7d91f737d9fb4467477aa
Yogthos <yogthos@gmail.com>
2025-07-31 17:53:50 -0400

    fix: update server port to 3001 and serve static files from models directory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Serve static files from the 'models' directory
 app.use('/models', express.static(path.join(__dirname, '../models')));
